b6dd776398e39af781da89a18d33e612b58f97c2
Ben Bariteau <bariteau@yelp.com>
2022-10-24 14:29:18 -0700

    fix setup.py for multi-module setup

--- setup.py
+++ setup.py
@@ -1,18 +1,8 @@
-try:
-    from setuptools import setup
-
-    setup  # shh, pyflakes
-
-    setuptools_kwargs = {
-        "provides": ["dateglob"],
-        "test_suite": "tests",
-    }
-except ImportError:
-    from distutils.core import setup
-
-    setuptools_kwargs = {}
+from setuptools import find_packages
+from setuptools import setup
 
 setup(
+    packages=find_packages(exclude=["tests*"]),
     author="David Marin",
     author_email="dm@davidmarin.org",
     classifiers=[
@@ -33,11 +23,9 @@ setup(
     license="Apache",
     long_description=open("README.rst").read(),
     name="dateglob",
-    py_modules=["dateglob"],
     url="http://github.com/Yelp/dateglob",
     version="1.1.0",
     package_data={
         "dateglob": ["py.typed"],
     },
-    **setuptools_kwargs
 )
